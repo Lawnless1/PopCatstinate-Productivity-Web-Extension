1cb51ea95641c469bf0932b85b39cb963469a3cc
Eric Deng <eric.rrdeng@gmail.com>
2023-10-01 06:52:22 -0400

    fregregre

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from process import processBrowserHistory
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
+CORS(app, supports_credentials=True)
 @app.route("/lol", methods = ["GET"])
 def lol():
     return "rip bozo"
 
 
 @app.route("/process", methods = ["GET"])
+@cross_origin(supports_credentials=True)
 def processHistory():
     return processBrowserHistory("{\"a\":1}")
 
